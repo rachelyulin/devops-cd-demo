b5bf4ece14fd94c8081f7bfa8b542b480c8e1184
rachel <rachel.yulin@gmail.com>
2023-08-14 20:28:40 +0800

    Fail the test

--- sum.test.js
+++ sum.test.js
@@ -1,25 +1,26 @@
 const targetFn = require("./sum");
 
 test("1 + 1 = 2", () => {
-    // Condition
-    const n1 = 1;
-    const n2 = 1;
+  // Condition
+  const n1 = 1;
+  const n2 = 1;
 
-    // Trigger Test
-    const result = targetFn(n1, n2);
+  // Trigger Test
+  const result = targetFn(n1, n2);
 
-    // Assert result
-    expect(result).toBe(2); // To fail the test, change update code to `.toBe(3)`
-})
+  // Assert result
+  // expect(result).toBe(2); // To fail the test, change update code to `.toBe(3)`
+  expect(result).toBe(3); // Add this line
+});
 
 test("1 + a = null", () => {
-    // Condition
-    const n1 = 1;
-    const n2 = "a";
+  // Condition
+  const n1 = 1;
+  const n2 = "a";
 
-    // Trigger Test
-    const result = targetFn(n1,n2);
-    
-    // Assert result
-    expect(result).toBeNaN();
-})
+  // Trigger Test
+  const result = targetFn(n1, n2);
+
+  // Assert result
+  expect(result).toBeNaN();
+});
